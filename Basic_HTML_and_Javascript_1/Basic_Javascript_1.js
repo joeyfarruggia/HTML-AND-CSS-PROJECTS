24f3a433b8c4bfb64e2cbcb0bb2d866ae16044bb
joeyfarruggia <joeyfarruggia66@gmail.com>
2026-01-14 19:00:26 -0800

    Changes Made
    Multiple changes made to html 1 and 2 docs, as well as javascript 1 doc

--- Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -1,6 +1,8 @@
-<!DOCTYPE HTML>
-<html>
-<script>
 alert("Hello, Beautiful People.");
-</script>
-</html>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+	
