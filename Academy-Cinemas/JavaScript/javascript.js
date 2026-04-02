90b8afa9979fa0a0e51d3000966dac95cd8f4230
joeyfarruggia <joeyfarruggia66@gmail.com>
2026-04-02 13:52:19 -0700

    Academy-Cinemas

---
+++ Academy-Cinemas/JavaScript/javascript.js
@@ -0,0 +1,48 @@
+//Initialize tooltips
+//Adds a popup tooltip to an element when the user hovers over it, providing additional information.
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el))
+
+//Initialize popovers
+//Adds a popup box that displays additional content when the user clicks or hovers over an element. 
+//In this case, it displays an image of the star rating for the selected movie.
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+
+popoverTriggerList.forEach(function (element) {
+    var imgSrc = element.getAttribute('data-bs-img');
+    var content = "<img class='star-rating' src='" + imgSrc + "'>";
+    new bootstrap.Popover(element, {
+        content: content,
+        html: true,
+        trigger: 'hover',
+    });
+});
+
+//Inititalize toast
+//A toast is a small, non-intrusive message that appears on the screen 
+//to provide feedback or information to the user.
+
+const toastElList = document.querySelectorAll('.toast')
+const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl))
+
+//Function to display toast with selected options
+
+function displaySelectedMovieOptions() {
+    var movie = document.getElementById('movieSelect').options[document.getElementById('movieSelect').selectedIndex].text;
+    var time = document.getElementById('timeSelect').options[document.getElementById('timeSelect').selectedIndex].text;
+    var quantity = document.getElementById('quantity').value;
+
+    var message = "Purchase confirmed for " + movie + " \nTime: " + time + " \nTickets: " + quantity + ".";
+
+    //Display toast
+    var toastBody = document.getElementById('toastBody');
+    toastBody.textContent = message;
+    var toast = new bootstrap.Toast(document.getElementById('toastDisplay'));
+    toast.show();
+}
+
+function buyTickets() {
+    displaySelectedMovieOptions();
+}
