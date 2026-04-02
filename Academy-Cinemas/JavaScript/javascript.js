c6934f2317332e28628cbe836d7f258e496977c1
joeyfarruggia <joeyfarruggia66@gmail.com>
2026-04-02 14:06:50 -0700

    Academy-Cinemas

--- Academy-Cinemas/JavaScript/javascript.js
+++ Academy-Cinemas/JavaScript/javascript.js
@@ -46,3 +46,37 @@ function displaySelectedMovieOptions() {
 function buyTickets() {
     displaySelectedMovieOptions();
 }
+
+//JQUERY
+
+//Shrinks header size when the document is scrolled down by 80 pixels.
+$(document).on("scroll", function () {
+    //When the webpage is scrolled down from the top by 50px this
+    //if statement will be triggered.
+    if ($(document).scrollTop() > 50) {
+        //once the 50px requirement is met, add the nav-shrink class
+        //selector to the same HTML element that has the nav class
+        $("nav").addClass("nav-shrink");
+        ////Adjust the position of the mobile drop down menu
+        //to accomodate the new height decrease
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        //if the webpage has not been scrolled down or is back at the top,
+        //the nav-shrink class selector is removed from the HTML element
+        //with the nav class selector.
+        $("nav").removeClass("nav-shrink");
+        //The margin for the drop down menu is now returned to its original position
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+});
+
+//Close mobile menu when a navigation link is clicked
+$(document).ready(function () {
+    //On click when an element contains just the nav-link class and not the 
+    //dropdown-toggle and then also close when an element with the class 
+    //.dropdown-item (each movie link) is clicked
+    $(".navbar-nav").on("click", ".nav-link:not(.dropdown-toggle), .dropdown-item", function () {
+        //Collapse the navbar when a link or dropdown item is clicked
+        $(".navbar-collapse").collapse("hide");
+    });
+});
